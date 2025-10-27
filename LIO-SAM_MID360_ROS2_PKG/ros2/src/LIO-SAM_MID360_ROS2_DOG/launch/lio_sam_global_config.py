88f747e4f290341916903442dc23586923cb1568
ywj <ypat_999@163.com>
2025-10-27 14:23:34 +0800

    push rotate msg

--- LIO-SAM_MID360_ROS2_PKG/ros2/src/LIO-SAM_MID360_ROS2_DOG/launch/lio_sam_global_config.py
+++ LIO-SAM_MID360_ROS2_PKG/ros2/src/LIO-SAM_MID360_ROS2_DOG/launch/lio_sam_global_config.py
@@ -31,7 +31,7 @@ config_by_machine = {
         'ONLINE_LIDAR': False,  # jqr001可能主要用于离线数据处理
         'BASE_CODE_PATH': '/home/ywj/projects/git/dog_slam/LIO-SAM_MID360_ROS2_PKG/ros2/src/LIO-SAM_MID360_ROS2_DOG/',
         # 'DEFAULT_BAG_PATH': '/home/ywj/projects/dataset/robot/livox_record_new/',
-        'DEFAULT_BAG_PATH': '/home/ywj/projects/dataset/robot/livox_record_tilt_begin/',
+        'DEFAULT_BAG_PATH': '/home/ywj/projects/dataset/robot/livox_record_tilt_new/',
         'DEFAULT_RELIABILITY_OVERRIDE': '/home/ywj/projects/dataset/reliability_override.yaml',
         'DEFAULT_LOAM_SAVE_DIR': '/home/ywj/projects/LOAM/',
         'DEFAULT_MAP_FILE': '/home/ywj/projects/map_grid/map.yaml',

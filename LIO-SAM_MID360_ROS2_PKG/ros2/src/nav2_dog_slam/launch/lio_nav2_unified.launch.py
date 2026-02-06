c60e1e1daa6cb62d45e286919bb9d34e2f49f4cd
rk <rk@ropeok.com>
2026-02-06 18:10:20 +0800

    参数微调，superlio疑似会影响odom使其倾斜，需调试

--- LIO-SAM_MID360_ROS2_PKG/ros2/src/nav2_dog_slam/launch/lio_nav2_unified.launch.py
+++ LIO-SAM_MID360_ROS2_PKG/ros2/src/nav2_dog_slam/launch/lio_nav2_unified.launch.py
@@ -269,10 +269,10 @@ def generate_launch_description():
         parameters=[{
             'transform_tolerance': 0.1,
             'min_height': -0.1,
-            'max_height': 1.5,
+            'max_height': 1.0,
             'angle_min': -3.1,
             'angle_max': 3.1,
-            'angle_increment': 0.00869347338,
+            'angle_increment': 0.05, #0.00869347338,
             'scan_time': 0.1,
             'range_min': 0.3,
             'range_max': 100.0,
